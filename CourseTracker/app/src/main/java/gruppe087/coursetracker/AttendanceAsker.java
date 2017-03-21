57318e2a395071d3fae95ca05f74629e138ae7b4
Henrik <henrikbossart@me.com>
2017-03-21 20:08:45 +0100

    started working on notifications

---
+++ CourseTracker/app/src/main/java/gruppe087/coursetracker/AttendanceAsker.java
@@ -0,0 +1,13 @@
+package gruppe087.coursetracker;
+
+import android.support.v7.app.NotificationCompat;
+
+/**
+ * Created by henrikbossart on 21.03.2017.
+ */
+
+public class AttendanceAsker {
+
+    NotificationCompat.Builder mBuilder = new NotificationCompat().Builder(this);
+
+}
