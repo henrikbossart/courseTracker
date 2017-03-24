17f2f6784ed9d83368feb6fc7970352a20037a2c
Henrik <henrikbossart@me.com>
2017-03-24 20:14:34 +0100

    notifications are easy to implement, have to merge with dev to get newest update

--- CourseTracker/app/src/main/java/gruppe087/coursetracker/ChooseCourseAtSetupActivity.java
+++ CourseTracker/app/src/main/java/gruppe087/coursetracker/ChooseCourseAtSetupActivity.java
@@ -1,11 +1,15 @@
 package gruppe087.coursetracker;
 
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteConstraintException;
 import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
@@ -47,7 +51,27 @@ public class ChooseCourseAtSetupActivity extends AppCompatActivity {
     SharedPreferences settings;
     String username;
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public void sendNotification() {
 
+        Intent intent = new Intent(this, NotificationReceiverActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, (int)
+            System.currentTimeMillis(), intent, 0);
+
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle("Course Tracker")
+                .setContentText("Did you attend today lecture in *****?")
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+        notificationManager.notify(0, notification);
+
+    }
 
 
     @Override
@@ -132,6 +156,7 @@ public class ChooseCourseAtSetupActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Define action on click
+                sendNotification();
                 ArrayList<String> courseCodes = new ArrayList<String>();
                 for (int i : selected){
                     String courseCode = overview_list.get(i).split(" ")[0];
