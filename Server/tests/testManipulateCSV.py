7b7dadf32340d4a2011ae324a6d13fd1f1220cb8
Aleksander <aleksahh@stud.ntnu.no>
2017-04-24 15:21:27 +0200

    Test coverage - csv manipulation

--- Server/tests/testManipulateCSV.py
+++ Server/tests/testManipulateCSV.py
@@ -1,51 +1,87 @@
 # encoding: utf-8
+import unittest2
+import os
 
+from scraper.scrapeLectureInformation import LectureInformationScraper
 from scraper.manipulateCSV import ManipulateCSVFile
 
+
 class TestManipulateCSVFile:
-  
-    # Testing everything
-    #-----------------------------
+
+
+    # Add filepaths!
     def __init__(self):
-        self.test = ManipulateCSVFile()
-        self.choice = None
 
-    def chooseOption(self, file):
-        string = ("What do you wan|t to do with the csv file? \n0: Nothing -> Exit \n1: Write \n2: Read \n3: Count lines in file \n4: Delete everything inside the file \nChoice: ")
+        self.testTemp = '/home/aleksanderhh/Downloads/Database/tests/temp.csv'
+        # Init manipulateCSV
+        self.self.testMCSV = ManipulateCSVFile()
+
+
+    # Test: Commandline for choosing file to manipulate
+    def testChooseFile(self):
+        assertEqual(self.testMCSV.chooseFile(0), self.testMCSV.lecture)
+        assertEqual(self.testMCSV.chooseFile(1), self.testMCSV.testTdtCourses)
+        assertEqual(self.testMCSV.chooseFile(2), self.testMCSV.courseCoordinator)
+        assertEqual(self.testMCSV.chooseFile(3), self.testMCSV.lecturer)
+        assertEqual(self.testMCSV.chooseFile(4), self.testMCSV.semester)
+        assertEqual(self.testMCSV.chooseFile(5), self.testMCSV.lectureFixed)
+        assertNotEqual(self.testMCSV.chooseFile(6), -1)
+
+
+    # Test: Returns TDT courses csv filepath
+    def testGetCourseCodeFilePath(self):
+
+        assertEqual(self.testMCSV.testTdtCourses, )
+
+
+    # Test: Prints out content in csv file
+    def testReadCSV(self):
+
+        self.testMCSV.readCSV(self.testMCSV.tdtCourses)
+
+
+    # Test: Fetch and saves a specific column in csv file to a list
+    def testFetchFromCSV(self):
+
+        assertNotEqual(self.testMCSV.fetchFromCSV(self.testMCSV.tdtCourses, 0), "")
     
-        while(self.choice != 0):
-            self.choice = int(raw_input(string))
+
+    # Test: Writes to csv file
+    def testWriteToCSV(self):
+
+        sizeBefore = self.testMCSV.tdtCourses.st_size
+        self.testMCSV.writeToCSV(self.testMCSV.tdtCourses, "TEST")
+        assertNotEqual(sizeBefore, self.testMCSV.tdtCourses.st_size)
         
-            if(self.choice == 0):
-                #0 - Exiting
-                print "\nExiting"   
 
-            elif(self.choice == 1):
-                #1 - Writing to file
-                self.test.writeToCSV(file, "Test1, \nTest2, \nTest3, ")
-                print "\nTested writing to csv file"
-         
-            elif(self.choice == 2):
-                #2 - Reading file
-                self.test.readCSV(file)
-                print "\nTested reading csv file"
-         
-            elif(self.choice == 3):
-                #3 - Count lines in file
-                num = self.test.countLinesInCSV(file)
-                print ("\nNumber of lines in the file is: " + num)
-            
-            elif(self.choice == 4):
-                #4 - Delete everything inside the file
-                self.test.cleanCSVFile(file)
-                print "\nTest file is now empty"
-         
-            else:
-                print "\nYou didn't choose any of the options"
+    # Test: Count lines in csv file
+    def testCountLinesInCSV(self):
 
-            #return choice
+        assertEqual(self.testMCSV.countLinesInCSV(self.testTemp), 1)
 
-    def run(self):
-        file = self.test.chooseFile()
-        self.chooseOption(file)
 
+    # Test: Deletes information stored in the csv-file
+    def testCleanCSVFile(self):
+
+        self.testMCSV.cleanCSVFile(self.testTemp)
+        assertEqual(self.testTemp.st_size, 0)
+
+
+    # Test: Makes the csv file correct for implementation to sql database
+    def testFixtestLectureWeeks(self):
+
+        self.testMCSV.fixLectureWeeks()
+        assertNotEqual(self.testMCSV.lectureFixed.st_size, 0)
+
+
+    # Returns date from weekday, weeknumber and year
+    def testGetDate(self):
+    
+        assertEqual(self.testMCSV.getDate(3,11), '2017-03-16')
+    
+
+    # Convert weekday and week to date
+    def testLectureToDateFormat(self):
+        
+        assertNotEqual(self.testMCSV.lectureFixedDate.st_size, 0)
+        
--- Server/tests/testScrapeCourseInformation.py
+++ Server/tests/testScrapeCourseInformation.py
@@ -2,11 +2,16 @@
 
 from scraper.manipulateCSV import ManipulateCSVFile
 
+# This is an example on how we can scrape information about lectures from NTNU's site.
 
-class CourseInformationScraperTest:
-
+class CourseInformationScraper:
 
     def __init__(self):
 
 
 
+
+    # Reads courseCodes from csv file
+    def getCourseInformationLink(self):
+        
+        self.courseInformationLink = url
