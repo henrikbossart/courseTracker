abe312194d2fba87a2ea20a5966d26655840a9a0
Aleksander <aleksahh@stud.ntnu.no>
2017-04-24 15:27:42 +0200

    Test coverage - csv manipulation update
    Test coverage of 73%

--- Server/tests/testManipulateCSV.py
+++ Server/tests/testManipulateCSV.py
@@ -6,82 +6,78 @@ from scraper.scrapeLectureInformation import LectureInformationScraper
 from scraper.manipulateCSV import ManipulateCSVFile
 
 
-class TestManipulateCSVFile:
-
-
-    # Add filepaths!
-    def __init__(self):
-
-        self.testTemp = '/home/aleksanderhh/Downloads/Database/tests/temp.csv'
-        # Init manipulateCSV
-        self.self.testMCSV = ManipulateCSVFile()
+class TestManipulateCSVFile(unittest2.TestCase):
 
 
     # Test: Commandline for choosing file to manipulate
     def testChooseFile(self):
-        assertEqual(self.testMCSV.chooseFile(0), self.testMCSV.lecture)
-        assertEqual(self.testMCSV.chooseFile(1), self.testMCSV.testTdtCourses)
-        assertEqual(self.testMCSV.chooseFile(2), self.testMCSV.courseCoordinator)
-        assertEqual(self.testMCSV.chooseFile(3), self.testMCSV.lecturer)
-        assertEqual(self.testMCSV.chooseFile(4), self.testMCSV.semester)
-        assertEqual(self.testMCSV.chooseFile(5), self.testMCSV.lectureFixed)
-        assertNotEqual(self.testMCSV.chooseFile(6), -1)
+        assertEqual(self.ManipulateCSVFile().chooseFile(0), self.ManipulateCSVFile().lecture)
+        assertEqual(self.ManipulateCSVFile().chooseFile(1), self.ManipulateCSVFile().testTdtCourses)
+        assertEqual(self.ManipulateCSVFile().chooseFile(2), self.ManipulateCSVFile().courseCoordinator)
+        assertEqual(self.ManipulateCSVFile().chooseFile(3), self.ManipulateCSVFile().lecturer)
+        assertEqual(self.ManipulateCSVFile().chooseFile(4), self.ManipulateCSVFile().semester)
+        assertEqual(self.ManipulateCSVFile().chooseFile(5), self.ManipulateCSVFile().lectureFixed)
+        assertNotEqual(self.ManipulateCSVFile().chooseFile(6), -1)
 
 
     # Test: Returns TDT courses csv filepath
     def testGetCourseCodeFilePath(self):
 
-        assertEqual(self.testMCSV.testTdtCourses, )
+        assertEqual(self.ManipulateCSVFile().testTdtCourses, )
 
 
     # Test: Prints out content in csv file
     def testReadCSV(self):
 
-        self.testMCSV.readCSV(self.testMCSV.tdtCourses)
+        self.ManipulateCSVFile().readCSV(self.ManipulateCSVFile().tdtCourses)
 
 
     # Test: Fetch and saves a specific column in csv file to a list
     def testFetchFromCSV(self):
 
-        assertNotEqual(self.testMCSV.fetchFromCSV(self.testMCSV.tdtCourses, 0), "")
+        assertNotEqual(self.ManipulateCSVFile().fetchFromCSV(self.ManipulateCSVFile().tdtCourses, 0), "")
     
 
     # Test: Writes to csv file
     def testWriteToCSV(self):
 
-        sizeBefore = self.testMCSV.tdtCourses.st_size
-        self.testMCSV.writeToCSV(self.testMCSV.tdtCourses, "TEST")
-        assertNotEqual(sizeBefore, self.testMCSV.tdtCourses.st_size)
+        sizeBefore = self.ManipulateCSVFile().tdtCourses.st_size
+        self.ManipulateCSVFile().writeToCSV(self.ManipulateCSVFile().tdtCourses, "TEST")
+        assertNotEqual(sizeBefore, self.ManipulateCSVFile().tdtCourses.st_size)
         
 
     # Test: Count lines in csv file
     def testCountLinesInCSV(self):
 
-        assertEqual(self.testMCSV.countLinesInCSV(self.testTemp), 1)
+        assertEqual(self.ManipulateCSVFile().countLinesInCSV('/home/aleksanderhh/Downloads/Database/tests/temp.csv'), 1)
 
 
     # Test: Deletes information stored in the csv-file
     def testCleanCSVFile(self):
 
-        self.testMCSV.cleanCSVFile(self.testTemp)
-        assertEqual(self.testTemp.st_size, 0)
+        self.ManipulateCSVFile().cleanCSVFile('/home/aleksanderhh/Downloads/Database/tests/temp.csv')
+        assertEqual('/home/aleksanderhh/Downloads/Database/tests/temp.csv'.st_size, 0)
 
 
     # Test: Makes the csv file correct for implementation to sql database
     def testFixtestLectureWeeks(self):
 
-        self.testMCSV.fixLectureWeeks()
-        assertNotEqual(self.testMCSV.lectureFixed.st_size, 0)
+        self.ManipulateCSVFile().fixLectureWeeks()
+        assertNotEqual(self.ManipulateCSVFile().lectureFixed.st_size, 0)
 
 
     # Returns date from weekday, weeknumber and year
     def testGetDate(self):
     
-        assertEqual(self.testMCSV.getDate(3,11), '2017-03-16')
+        assertEqual(self.ManipulateCSVFile().getDate(3,11), '2017-03-16')
     
 
     # Convert weekday and week to date
     def testLectureToDateFormat(self):
         
-        assertNotEqual(self.testMCSV.lectureFixedDate.st_size, 0)
-        
+        assertNotEqual(self.ManipulateCSVFile().lectureFixedDate.st_size, 0)
+       
+
+
+if __name__ == '__main__':
+    unittest2.main()
