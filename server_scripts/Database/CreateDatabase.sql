54ff5a370a7842ac4a4b911af205ed66a0bb17d6
Aleksander <aleksahh@stud.ntnu.no>
2017-04-27 08:58:12 +0200

    Updated with table User
    Updated CreateDatabase.sql with the table «User»

--- server_scripts/Database/CreateDatabase.sql
+++ server_scripts/Database/CreateDatabase.sql
@@ -1,27 +1,32 @@
+CREATE TABLE User(
+id          INT(10)     NOT NULL    PRIMARY KEY,
+username    VARCHAR(25) NOT NULL,
+password    VARCHAR(20) NOT NUll);
+
 CREATE TABLE Semester(
-id		INT(3)		NOT NULL	PRIMARY KEY,
-year		TIMESTAMP	NOT NULL,
-type		CHAR(6)		NOT NULL);
+id          INT(3)      NOT NULL    PRIMARY KEY,
+year        TIMESTAMP   NOT NULL,
+type        CHAR(6)     NOT NULL);
 
 CREATE TABLE CourseCoordinator(
-mail		VARCHAR(30)	NOT NULL	PRIMARY KEY,
-name		VARCHAR(20),
-courseCode	CHAR(7)		NOT NULL	FOREIGN KEY);
+mail        VARCHAR(30) NOT NULL    PRIMARY KEY,
+name        VARCHAR(20),
+courseCode  CHAR(7)     NOT NULL    FOREIGN KEY);
 
 CREATE TABLE Course(
-CourseCode	CHAR(7) 	NOT NULL	 PRIMARY KEY,
-CourseName	VARCHAR(40),
-location	VARCHAR(20),
-department	VARCHAR(20));
+CourseCode  CHAR(7)     NOT NULL    PRIMARY KEY,
+CourseName  VARCHAR(40),
+location    VARCHAR(20),
+department  VARCHAR(20));
 
 CREATE TABLE Lecture(
-CourseCode	CHAR(7)		NOT NULL	PRIMARY KEY,
-weekDay	 	INT(1) 		NOT NULL,
-WeekNum		INT(2)		NOT NULL,
-StartTime	CHAR(5)		NOT NULL,
-Room		CHAR(3)		NOT NULL);
+CourseCode  CHAR(7)     NOT NULL    PRIMARY KEY,
+weekDay     INT(1)      NOT NULL,
+WeekNum     INT(2)      NOT NULL,
+StartTime   CHAR(5)     NOT NULL,
+Room        CHAR(3)     NOT NULL);
 
 CREATE TABLE Lecturer(
-mail		VARCHAR(30)	NOT NULL	PRIMARY KEY,
-name		VARCHAR(20),
-courseCode	CHAR(7)		NOT NULL	FOREIGN KEY);
+mail        VARCHAR(30) NOT NULL    PRIMARY KEY,
+name        VARCHAR(20),
+courseCode  CHAR(7)     NOT NULL    FOREIGN KEY);
